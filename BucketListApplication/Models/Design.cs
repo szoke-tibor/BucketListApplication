2cbef426c34cfb0751b946c03eac5e7aaa8189dc
Tibor Ádám Szőke <szoketibi97@sch.bme.hu>
2019-11-12 16:32:32 +0100

    Design class changes BorderType enum moved into Design class Colors are not Mapped anymore

--- BucketListApplication/Models/Design.cs
+++ BucketListApplication/Models/Design.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,8 +11,22 @@ namespace BucketListApplication.Models
     {
         public int DesignID { get; set; }
         public string PictureURL { get; set; }
+		public Int32 BorderColorARGB
+		{
+			get { return BorderColor.ToArgb(); }
+			set { BorderColor = Color.FromArgb(value); }
+		}
+
+		public Int32 BackgroundColorARGB
+		{
+			get { return BackgroundColor.ToArgb(); }
+			set { BackgroundColor = Color.FromArgb(value); }
+		}
+
+		[NotMapped]
         public Color BorderColor { get; set; }
-        public Color BackgroundColor { get; set; }
+		[NotMapped]
+		public Color BackgroundColor { get; set; }
 
         public BorderType BorderType { get; set; }
     }
