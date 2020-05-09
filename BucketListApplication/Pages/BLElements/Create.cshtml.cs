f78098c74cfacbdf8c4b1f98bd6e9d6b58188bfd
Tibor Ádám Szőke <szoketibi97@sch.bme.hu>
2020-05-09 20:43:34 +0200

    Added category for Creating BLEelement form

--- BucketListApplication/Pages/BLElements/Create.cshtml.cs
+++ BucketListApplication/Pages/BLElements/Create.cshtml.cs
@@ -37,10 +37,13 @@ namespace BucketListApplication.Pages.BLElements
 				throw new Exception("Nincs bejelentkezett felhasználó.");
 
 			ViewData["Design"] = new SelectList(_context.Designs, nameof(Models.Design.DesignID), nameof(Models.Design.Name));
-            return Page();
+			ViewData["Category"] = new SelectList(_context.Categories, nameof(Models.Category.CategoryID), nameof(Models.Category.Name));
+			return Page();
         }
 
-        [BindProperty]
+		[BindProperty]
+		public Category Category { get; set; }
+		[BindProperty]
         public BucketListElement BucketListElement { get; set; }
 
 		// DONE
@@ -48,18 +51,40 @@ namespace BucketListApplication.Pages.BLElements
         // more details see https://aka.ms/RazorPagesCRUD.
         public async Task<IActionResult> OnPostAsync()
         {
-			var emptyBucketListElement = new BucketListElement();
+			ElementCategory ec = new ElementCategory();
 
+			var emptyBucketListElement = new BucketListElement();
+			var emptyElementCategory = new ElementCategory();
+
+			// Defense against overposting attacks. Returns true if the update was successful.
 			if (await TryUpdateModelAsync<BucketListElement>(
 				emptyBucketListElement,
-				"bucketListElement",   // Prefix for form value.
+				"BucketListElement",   // Prefix for form value.
 				ble => ble.Name, ble => ble.DesignID,
                 ble => ble.BucketListID, ble => ble.Description, ble => ble.Completed, ble => ble.Visibility))
 			{
 				_context.Elements.Add(emptyBucketListElement);
 				await _context.SaveChangesAsync();
+
+				// Searching the added BLElement to get its ElementID
+				BucketListElement addedBLElement = _context.BLElements.Where(bl => bl.Name == emptyBucketListElement.Name)
+																	  .Where(bl => bl.DesignID == emptyBucketListElement.DesignID)
+																	  .Where(bl => bl.BucketListID == emptyBucketListElement.BucketListID)
+																	  .Where(bl => bl.Description == emptyBucketListElement.Description)
+																	  .Where(bl => bl.Completed == emptyBucketListElement.Completed)
+																	  .Where(bl => bl.Visibility == emptyBucketListElement.Visibility)
+																	  .First();
+
+				Category selectedCategory = _context.Categories.Where(c => c.CategoryID == Category.CategoryID).First();
+
+				ec.ElementID = addedBLElement.ElementID;
+				ec.CategoryID = selectedCategory.CategoryID;
+				_context.ElementCategories.Add(ec);
+				await _context.SaveChangesAsync();
+
 				return RedirectToPage("Index");
 			}
+
 			return Page();
 		}
     }
