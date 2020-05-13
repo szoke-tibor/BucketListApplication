d1276e4d898525395fcb3d90d4771d7756de05c2
Tibor Ádám Szőke <szoketibi97@sch.bme.hu>
2020-05-13 02:36:21 +0200

    BLElements can be edited (TODO: Categories)

--- BucketListApplication/Pages/BLElements/Edit.cshtml.cs
+++ BucketListApplication/Pages/BLElements/Edit.cshtml.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using BucketListApplication.Data;
 using BucketListApplication.Models;
+using System.Security.Claims;
 
 //Not yet implemented
 namespace BucketListApplication.Pages.BLElements
@@ -15,28 +16,51 @@ namespace BucketListApplication.Pages.BLElements
     public class EditModel : PageModel
     {
         private readonly BucketListApplication.Data.BLContext _context;
+        public SelectList DesignSelect { get; set; }
+        public SelectList CategorySelect { get; set; }
+        public SelectList BLSelect { get; set; }
+
+        [BindProperty]
+        public int[] SelectedCategories { get; set; }
+        [BindProperty]
+        public BucketListElement BLElement { get; set; }
 
         public EditModel(BucketListApplication.Data.BLContext context)
         {
             _context = context;
         }
 
-        [BindProperty]
-        public BucketListElement BLElement { get; set; }
-
         public async Task<IActionResult> OnGetAsync(int? id)
         {
-            if (id == null)
+            //Logged user's userId
+            var CurrentUserId = _context._httpContextAccessor.HttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (CurrentUserId != null)
             {
-                return NotFound();
-            }
+                //Logged user's BucketLists
+                var CurrentUsersBucketLists = from bl in _context.BucketLists
+                                              where bl.UserId == CurrentUserId
+                                              select bl;
 
-            BLElement = await _context.BLElements.FindAsync(id);
+                //SelectLists
+                BLSelect = new SelectList(CurrentUsersBucketLists, nameof(Models.BucketList.BucketListID), nameof(Models.BucketList.Name));
+                DesignSelect = new SelectList(_context.Designs, nameof(Models.Design.DesignID), nameof(Models.Design.Name));
+                CategorySelect = new SelectList(_context.Categories, nameof(Models.Category.CategoryID), nameof(Models.Category.Name));
 
-            if (BLElement == null)
-            {
-                return NotFound();
+                //Searching for the BLElement
+                if (id == null) { return NotFound(); }
+                BLElement = await _context.BLElements.FindAsync(id);
+                if (BLElement == null) { return NotFound(); }
+
+                //Setting the active Categories in the SelectList
+                int CategoryCount = _context.Categories.Count();
+                SelectedCategories = new int[CategoryCount];
+                var ElementCategories = _context.ElementCategories.Where(ec => ec.ElementID == BLElement.ElementID).ToList();
+                for (int i = 0; i < ElementCategories.Count(); i++)
+                    SelectedCategories[i] = ElementCategories.ElementAt(i).CategoryID;
             }
+            else
+                throw new Exception("Nincs bejelentkezett felhasználó.");
+
             return Page();
         }
 
@@ -55,11 +79,16 @@ namespace BucketListApplication.Pages.BLElements
 			if (await TryUpdateModelAsync<BucketListElement>(
 				elementToUpdate,
 				"blelement",
-				ble => ble.Name))
+                ble => ble.Name, ble => ble.DesignID,
+                ble => ble.BucketListID, ble => ble.Description, ble => ble.Completed, ble => ble.Visibility))
 			{
 				await _context.SaveChangesAsync();
 				return RedirectToPage("./Index");
 			}
+
+            //TODO
+            //managing ElementCategories
+            //Setting navigation properties
 
 			return Page();
 		}
