d8b9f401a43a67bb2f422ff5b3cd21332a405cad
Tibor Ádám Szőke <szoke.tibi97@gmail.com>
2020-10-24 15:03:02 +0200

    BLElements now are listed right after selecting a BucketList

--- BucketListApplication/Pages/BLElements/BLElementListingPageModel.cs
+++ BucketListApplication/Pages/BLElements/BLElementListingPageModel.cs
@@ -19,12 +19,28 @@ namespace BucketListApplication.Pages.BLElements
 
 		public void PopulateBucketListDropDownList(BLContext _context, string UserId, object selectedBucketList = null)
 		{
+			List<SelectListItem> SelectListItems = new List<SelectListItem>();
+			SelectListItems.Add(new SelectListItem()
+			{
+				Text = "--Válassz egy listát--",
+				Value = "null"
+			});
+
 			var bucketListsQuery = from bl in _context.BucketLists
 								   where bl.UserId == UserId
 								   orderby bl.Name
-							       select bl;
+								   select bl;
 
-			BucketListSL = new SelectList(bucketListsQuery.AsNoTracking(), nameof(Models.BucketList.BucketListID), nameof(Models.BucketList.Name), selectedBucketList);
+			foreach (BucketList bl in bucketListsQuery)
+			{
+				SelectListItems.Add(new SelectListItem()
+				{
+					Text = bl.Name,
+					Value = bl.BucketListID.ToString()
+				});
+			}
+
+			BucketListSL = new SelectList(SelectListItems, "Value", "Text", null);
 		}
 
 		public void PopulateSelectedBLElementsList(BLContext _context, int SelectedBucketListID, bool PublicOnly)
