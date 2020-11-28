175a3981dd9c1fdd8550db5f8517ad8d27111d59
Tibor Ádám Szőke <szoke.tibi97@gmail.com>
2020-11-28 22:06:33 +0100

    Removed azure-pipelines

--- BucketListApplication/Startup.cs
+++ BucketListApplication/Startup.cs
@@ -27,7 +27,7 @@ namespace BucketListApplication
 		{
 			services.AddDbContext<BLContext>(options =>
 				options.UseSqlServer(
-					Configuration.GetConnectionString("AzureSQLserver")));
+					Configuration.GetConnectionString("BucketSQLserver")));
 
 			services.AddTransient<IUserService, UserService>();
 			services.AddTransient<IBucketListService, BucketListService>();
